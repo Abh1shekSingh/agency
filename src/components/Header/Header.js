22da5fe937541ef5e530f01d1ff9dd820625f5cb
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-06-10 12:32:24 +0530

    navbar responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "../../../styles/Header.module.css"
 import {FaMagento} from "react-icons/fa"
 import {GiHamburgerMenu} from "react-icons/gi"
+
 const Header = () => {
+
+  const [navbar, setNavbar] = useState(false);
+  function handleToggle(){
+
+      setNavbar(!navbar)
+      
+  }
+
   return (
     <div className={style.container}>
         <div className={style.logo}>
@@ -14,12 +23,24 @@ const Header = () => {
               <li>Home</li>
               <li>Services</li>
               <li>Team</li>
-              <li>Testimonial</li>
+              <li>Hire</li>
             </ul>
         </div>
         <div className={style.hamburger}>
             <p>Get Started</p>
-            <GiHamburgerMenu className={style.hamIcon} />
+            <GiHamburgerMenu onClick={handleToggle} className={style.hamIcon} />
+        </div>
+        <div className={navbar ? style.sidenavbar : style.closedNavbar}>
+          <div className={style.inSidebarlogo}>
+              <FaMagento className={style.inSidebarlogoIcon} />
+              <h3>Ninja Agents</h3>
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Hire</li>
+          </ul>
         </div>
     </div>
   )
