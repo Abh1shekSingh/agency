4b62f3bca945f5c3cadce534a71e66f04a0df629
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-06-11 17:42:29 +0530

    Folder structure

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -46,9 +46,9 @@ export const developers = [
 
 
 export const trustedPartners =[
-    {id:1,image:dribbble, author:"By Denny Albaz, CEO of Paypal", review:"“Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”"},
+    {id:1,image:uber, author:"By Denny Albaz, CEO of Paypal", review:"“Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”"},
     {id:2,image:google, author:"By Denny Albaz, CEO of Paypal", review:"“Qwilr Page we have made for them. Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a””"},
-    {id:3,image:uber, author:"By Denny Albaz, CEO of Paypal", review:"“Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”"},
+    {id:3,image:paypal, author:"By Denny Albaz, CEO of Paypal", review:"“Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”"},
     {id:4,image:microsoft, author:"By Denny Albaz, CEO of Paypal", review:"“Qwilr Page we have made for them. Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a”"},
-    {id:5,image:paypal, author:"By Denny Albaz, CEO of Paypal", review:"“Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”"},
+    {id:5,image:dribbble, author:"By Denny Albaz, CEO of Paypal", review:"“Whatever we create in Qwilr looks absolutely brilliant. It helps show our work in the best light, and is downright impressive when clients see a Qwilr Page we have made for them.”"},
 ];
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import Hero from "../components/Hero/Hero"
+import About from "../components/About/About"
+import Team from "../components/Team/Team"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Team />
+      <Footer />
+    </div>
+  )
+}
