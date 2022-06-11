cec770d1ff142a391555381ef912597dc56d3959
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-06-11 18:01:05 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://i.ibb.co'],
+  },
 }
-
-module.exports = nextConfig
